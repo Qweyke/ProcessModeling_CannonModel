003a2677da82e48fd464d9b57e9da64a0957779e
Qweyke <kettenkrad1337@yandex.ru>
2024-12-24 13:39:39 +0500

    done

--- src/main.py
+++ src/main.py
@@ -2,43 +2,75 @@ from matplotlib import pyplot as plt
 
 from model import Model
 
-SHOTS = 50000
+DISTANCE = 1000
+SHOTS = 40000
+INC_ANGLE = 30
+INC_ANGLE_DEV = 0.4
+SPEED_DEV = 0.4
+
 CHUNK = 100
 
-hits_list = Model.simulate_shots(distance=1000, inc_angle_exp_val=45, inc_angle_std_dev=1, speed_std_dev=1,
-                                 shots_qnty=SHOTS)
+TARGET_SIZE = 50
+
+DENSITY_INTERVAL = 50
+LEFT_INTERVAL = 400
+RIGHT_INTERVAL = 1400
+
+shots_list = Model.simulate_shots(distance=DISTANCE, inc_angle_exp_val=INC_ANGLE, inc_angle_std_dev=INC_ANGLE_DEV,
+                                  speed_std_dev=SPEED_DEV,
+                                  total_shots=SHOTS)
+
+hit_probability = Model.calculate_hit_probability(shots_list=shots_list, distance=DISTANCE, total_shots=SHOTS,
+                                                  target_size=TARGET_SIZE)
+print(f"Target hit probability: {hit_probability}")
+
 exp_vals = []
 stat_disps = []
 elem_chunks = []
+Model.fill_lists_for_plot(shots_list=shots_list, total_shots=SHOTS, exp_vals=exp_vals, stat_disps=stat_disps,
+                          elem_chunks=elem_chunks, chunk_size=CHUNK)
 
-next_elems_chunk = CHUNK
-while next_elems_chunk <= SHOTS:
-    exp_vals.append(Model.calculate_exp_val(hits_list[:next_elems_chunk], next_elems_chunk))
-    stat_disps.append(Model.calculate_stat_disp(hits_list[:next_elems_chunk], next_elems_chunk))
-    elem_chunks.append(next_elems_chunk)
-    next_elems_chunk += CHUNK
+hit_density = Model.calculate_hit_density(shots_list=shots_list, total_shots=SHOTS,
+                                          left_interval=LEFT_INTERVAL, right_interval=RIGHT_INTERVAL,
+                                          interval_size=DENSITY_INTERVAL)
 
 if len(elem_chunks) == len(exp_vals) == len(stat_disps):
+    # plt.figure(figsize=(18, 8))
 
-    plt.figure(figsize=(12, 6))  # window size
-
-    plt.subplot(1, 2, 1)
+    # Expected val
+    plt.figure(figsize=(12, 8))
+    # plt.subplot(1, 3, 1)
     plt.plot(elem_chunks, exp_vals, marker='o', label='Expected Value', color='blue')
     plt.title('Expected Value vs Chunk Size')
     plt.xlabel('Chunk Size')
     plt.ylabel('Expected Value')
     plt.grid(True)
     plt.legend()
+    plt.tight_layout()
 
-    plt.subplot(1, 2, 2)
+    # Statistical disp
+    plt.figure(figsize=(12, 8))
+    # plt.subplot(1, 3, 2)
     plt.plot(elem_chunks, stat_disps, marker='x', label='Statistical Dispersion', color='red')
     plt.title('Statistical Dispersion vs Chunk Size')
     plt.xlabel('Chunk Size')
     plt.ylabel('Statistical Dispersion')
     plt.grid(True)
     plt.legend()
+    plt.tight_layout()
+
+    # Density histogram
+    plt.figure(figsize=(12, 8))
+    # plt.subplot(1, 3, 3)
+    intervals = list(range(LEFT_INTERVAL, RIGHT_INTERVAL, DENSITY_INTERVAL))
+    plt.bar(intervals, hit_density, width=DENSITY_INTERVAL, align='edge', edgecolor='black', alpha=0.7)
+    plt.title('Hit Density Histogram')
+    plt.xlabel('Intervals')
+    plt.ylabel('Hit Density')
+    plt.grid(True)
 
     plt.tight_layout()
+
     plt.show()
 else:
     print("Error in arrs sizes")
--- src/model.py
+++ src/model.py
@@ -9,10 +9,6 @@ MAGIC2_FOR_RANDN = 6
 class Model:
 
     @staticmethod
-    def __simulate_shot_distance(speed: float, angle: float) -> float:
-        return pow(speed, 2) * sin(2 * angle) / G_FORCE
-
-    @staticmethod
     def __generate_rand(expected_value: float, standard_deviation: float) -> float:
         norm_rand = 0
         for i in range(MAGIC_FOR_RANDN):
@@ -22,12 +18,10 @@ class Model:
         return expected_value + standard_deviation * norm_rand
 
     @staticmethod
-    def simulate_shot(inc_angle_exp_val: float, distance: float, inc_angle_std_dev: float,
-                      speed_std_dev: float, do_print: bool) -> float:
+    def single_shot(inc_angle_exp_val: float, distance: float, inc_angle_std_dev: float,
+                    speed_std_dev: float, do_print: bool) -> float:
         speed_exp_value = sqrt((distance * G_FORCE) / sin(2 * radians(inc_angle_exp_val)))
         speed = Model.__generate_rand(speed_exp_value, speed_std_dev)
-        if do_print:
-            print(f"Current speed = {speed}")
 
         angle = Model.__generate_rand(inc_angle_exp_val, inc_angle_std_dev)
         if do_print:
@@ -35,27 +29,57 @@ class Model:
         angle = radians(angle)
         if do_print:
             print(f"Current angle in radians = {angle}")
+            print(f"Current speed = {speed}")
 
-        return Model.__simulate_shot_distance(speed, angle)
+        return pow(speed, 2) * sin(2 * angle) / G_FORCE
 
     @staticmethod
     def simulate_shots(inc_angle_exp_val: float, distance: float, inc_angle_std_dev: float,
-                       speed_std_dev: float, shots_qnty: int) -> list:
-        hits_list = []
-        for shots in range(shots_qnty):
-            hits_list.append(
-                Model.simulate_shot(distance=distance, inc_angle_exp_val=inc_angle_exp_val,
-                                    inc_angle_std_dev=inc_angle_std_dev, speed_std_dev=speed_std_dev,
-                                    do_print=False))
-        return hits_list
+                       speed_std_dev: float, total_shots: int) -> list:
+        shots_list = []
+        for shots in range(total_shots):
+            shots_list.append(
+                Model.single_shot(distance=distance, inc_angle_exp_val=inc_angle_exp_val,
+                                  inc_angle_std_dev=inc_angle_std_dev, speed_std_dev=speed_std_dev,
+                                  do_print=False))
+        return shots_list
 
     @staticmethod
-    def calculate_exp_val(hits_list: list, hits_qnty: int):
-        return sum(hits_list) / hits_qnty
+    def calculate_hit_probability(shots_list: list, total_shots: int, distance: float, target_size: float):
+        left_target_interval = distance - target_size / 2
+        right_target_interval = distance + target_size / 2
+        hit_count = len([hit for hit in shots_list if left_target_interval <= hit <= right_target_interval])
+        return hit_count / total_shots
 
     @staticmethod
-    def calculate_stat_disp(hits_list: list, hits_qnty: int):
+    def calculate_exp_val(shots_list_chunk: list, total_shots_in_chunk: int):
+        return sum(shots_list_chunk) / total_shots_in_chunk
 
-        exp_val = Model.calculate_exp_val(hits_list, hits_qnty)
-        sqr_dif_sum = sum((x - exp_val) ** 2 for x in hits_list)
-        return sqr_dif_sum / (hits_qnty - 1)
+    @staticmethod
+    def calculate_stat_disp(shots_list_chunk: list, total_shots_in_chunk: int):
+
+        exp_val = Model.calculate_exp_val(shots_list_chunk, total_shots_in_chunk)
+        sqr_dif_sum = sum((x - exp_val) ** 2 for x in shots_list_chunk)
+        return sqr_dif_sum / (total_shots_in_chunk - 1)
+
+    @staticmethod
+    def fill_lists_for_plot(shots_list: list, total_shots: int, exp_vals: list, stat_disps: list, elem_chunks: list,
+                            chunk_size: int):
+        next_elems_chunk = chunk_size
+        while next_elems_chunk <= total_shots:
+            exp_vals.append(Model.calculate_exp_val(shots_list[:next_elems_chunk], next_elems_chunk))
+            stat_disps.append(Model.calculate_stat_disp(shots_list[:next_elems_chunk], next_elems_chunk))
+            elem_chunks.append(next_elems_chunk)
+            next_elems_chunk += chunk_size
+
+    @staticmethod
+    def calculate_hit_density(shots_list: list, total_shots: int, left_interval: int, right_interval: int,
+                              interval_size: int) -> list:
+        hits_in_intervals = []
+        next_point = left_interval
+        while next_point < right_interval:
+            hits_in_intervals.append(
+                len([hit for hit in shots_list if next_point <= hit < next_point + interval_size]) / total_shots)
+            next_point += interval_size
+
+        return hits_in_intervals
